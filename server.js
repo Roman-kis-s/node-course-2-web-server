af2ae4472463035575175a4354725d5eb4dbc533
Roman <morientesjob@gmail.com>
2018-03-28 18:06:49 +0300

    Add projects page

--- server.js
+++ server.js
@@ -51,6 +51,13 @@ app.get('/bad', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+   res.render('projects.hbs', {
+       pageTitle: 'My projects',
+       pageText: 'There will be my projects'
+   }) ;
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
